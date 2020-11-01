4f979819a16784386a5f4d8706a86ed5bd62f3b8
Anthony Martinez <Martinezanthony213@gmail.com>
2020-10-31 22:44:29 -0700

    wireframe update

--- js/main.js
+++ js/main.js
@@ -5,14 +5,18 @@
 
 
 /*----- app's state (variables) -----*/
-
+let winner;
+let gameStatus;
+let playerX;
+let playerO;
 
 
 
 
 /*----- cached element references -----*/
 const containerEl = document.getElementById('container');
-
+const replayBtn = document.getElementById('resetBtn') 
+const boxes = document.querySelectorAll('#container.box')
 
 
 
@@ -24,9 +28,16 @@ const containerEl = document.getElementById('container');
 
 /*----- functions -----*/
 
+// init();
+
+//Creates the Boxes/Cells 
 for(let i = 0; i < 9 ; i++){
     let divEL = document.createElement('div');
     divEL.id = i;
     divEL.classList.add('box');
     containerEl.appendChild(divEL);
 }
+
+// function getGameStatus(){
+//     if()
+// }
